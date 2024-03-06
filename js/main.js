f5c0b016966fdb99840e7c6c10df7e21c3009b45
fangs <catfxngs@gmail.com>
2024-03-06 17:26:03 -0500

    started a reactive sidebar toggle using JS and CSS

--- js/main.js
+++ js/main.js
@@ -9,3 +9,21 @@ init();
 function init() {
   //console.log("running!");
 }
+
+// sidebar collapse button, pardon the terrible code
+document.getElementById('sidebar-button').addEventListener('click', function() {
+  let sideBar = document.getElementById('leftside');
+  sideBar.classList.toggle('collapse');
+  let sidebarTexts = sideBar.getElementsByClassName('sidebar-text');
+  for (let i = 0; i < sidebarTexts.length; i++) {
+    sidebarTexts[i].classList.toggle('collapse');
+  }
+
+  // change the funny appearance of le button
+  let button = document.getElementById('sidebar-button');
+  if (sideBar.classList.contains('collapse')) {
+    button.textContent = '>'; // text when sidebar is collapsed
+  } else {
+    button.textContent = '<'; // text when sidebar is expanded, so, not collapsed
+  }
+});
